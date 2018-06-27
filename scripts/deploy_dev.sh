5beee5519a1677510b1d09f74da73f6e9e6d178b
Richard Parry <richard.parry@esure.com>
2018-06-27 11:52:03 +0100

    Set storage class for dev and rename template files

--- scripts/deploy_dev.sh
+++ scripts/deploy_dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-aws s3 sync ./config s3://$S3_DEV_BUCKET_NAME/config --delete --profile dev
-aws s3 sync ./templates s3://$S3_DEV_BUCKET_NAME/templates --delete --profile dev
+aws s3 sync ./config s3://$S3_DEV_BUCKET_NAME/config --delete --profile dev --storage-class REDUCED_REDUNDANCY
+aws s3 sync ./templates s3://$S3_DEV_BUCKET_NAME/templates --delete --profile dev --storage-class REDUCED_REDUNDANCY
